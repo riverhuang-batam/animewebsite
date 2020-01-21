08b0ec87858e856f00325821cec017250916c2fd
riverhuang-batam <riverhuang011@gmail.com>
2020-01-21 16:28:07 +0700

    install moment & react-moment add date to reviewcard

--- src/components/reviewcard/reviewcard.js
+++ src/components/reviewcard/reviewcard.js
@@ -1,35 +1,43 @@
-import React from 'react' 
+import React from 'react'
 import {Card, CardBody} from 'reactstrap'
-
-const ReviewCard = ({review}) =>(
+import Moment from 'react-moment'
+const ReviewCard = ({review}) => (
     <Card className="mt-4">
         <CardBody>
-    <div key={review.mal_id} >
-                    <img alt={review.reviewer.image_url} src={review.reviewer.image_url} width="50px" height="50px"/>
-                        <span>{review.reviewer.username}</span>
-                        <p>
-                            {review.content}
-                        </p>
-                        <p>
-                        Overall: {review.reviewer.scores.overall}
-                        </p>
-                        <p>
-                        Story: {review.reviewer.scores.story}    
-                        </p>
-                        <p>
-                        Animation: {review.reviewer.scores.animation}
-                        </p>
-                        <p>
-                        Sound: {review.reviewer.scores.sound}    
-                        </p>
-                        <p>
-                        Character: {review.reviewer.scores.character}
-                        </p>
-                        <p>
-                        Enjoyment: {review.reviewer.scores.enjoyment}
-                        </p>
-                        </div>
-                        </CardBody>
-                        </Card>
+            <div key={review.mal_id}>
+                <img
+                    alt={review.reviewer.image_url}
+                    src={review.reviewer.image_url}
+                    width="50px"
+                    height="50px"/>
+                <span>{review.reviewer.username}, 
+                <Moment format='MM/DD/YYYY'>
+                {review.date}
+                </Moment>
+                </span>
+                <p>
+                    {review.content}
+                </p>
+                <p>
+                    Overall: {review.reviewer.scores.overall}
+                </p>
+                <p>
+                    Story: {review.reviewer.scores.story}
+                </p>
+                <p>
+                    Animation: {review.reviewer.scores.animation}
+                </p>
+                <p>
+                    Sound: {review.reviewer.scores.sound}
+                </p>
+                <p>
+                    Character: {review.reviewer.scores.character}
+                </p>
+                <p>
+                    Enjoyment: {review.reviewer.scores.enjoyment}
+                </p>
+            </div>
+        </CardBody>
+    </Card>
 )
 export default ReviewCard;
